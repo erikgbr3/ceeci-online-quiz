1d63ecbe184f771cf2a6cdc076c91dcde54f7cae
Marco572918346 <marcoantonio.07102@gmail.com>
2024-02-09 12:27:38 -0600

    Modal para agregar preguntas

---
+++ src/pages/quiz/index.js
@@ -0,0 +1,11 @@
+import AddQuestion from "@/components/modals/AddQuestion";
+function Quizes () {
+  return (
+    <div>
+    <h1>Hola</h1>
+    <AddQuestion />
+    </div>
+  );
+}
+
+export default Quizes;
