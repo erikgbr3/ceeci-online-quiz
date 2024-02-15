c073742cab89932611841f965a79d1de020c06bc
MichelValian <michel14.valian@gmail.com>
2024-02-15 01:16:44 -0600

    Autenticacion y gestion de usuarios

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,10 @@
 import React from "react";
 import { Paper, CardMedia, } from "@mui/material";
+import LoginPage from "./login";
 
 function Home() {
   return (
-    <Paper>
-      {/* <CardMedia
-        component="img"
-        alt="Imagen"
-        image="https://scontent-dfw5-2.xx.fbcdn.net/v/t39.30808-6/255926978_3258503637807867_1804716959916837386_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=3635dc&_nc_ohc=_B-eFyS30YcAX_DxCP4&_nc_ht=scontent-dfw5-2.xx&oh=00_AfDbDJPrZhXaUGc7nW_P2RtjS9sYaAkJ9nUW47Dvc14bHw&oe=65A58750"
-        sx={{
-          width: 1000,
-          margin: "auto",
-          display: "block",
-          borderRadius: 10,
-        }}
-      /> */}
-    </Paper>
+    <LoginPage></LoginPage>
   );
 }
 
