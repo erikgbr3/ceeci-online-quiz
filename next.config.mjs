24da6de9c182ecba9d44010480fde2937ddbd457
erikgbr3 <erikgomez9982@gmail.com>
2024-03-10 17:57:22 -0600

    next_secret

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+const secret = process.env.NEXTAUTH_SECRET;
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Otras configuraciones de Next.js aquí, si las tienes
+
+  // Configuración de NextAuth.js
+  env: {
+    NEXTAUTH_SECRET: secret
+  }
+};
+
 
 export default nextConfig;
